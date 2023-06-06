86696ec0855f31eef7493055d4ab3fe9aed951cd
Piyush061990 <piyushgupta.tech06@gmail.com>
2023-06-06 17:58:09 -0400

    Adding Files

---
+++ Readme.md
@@ -0,0 +1,59 @@
+Sure, here's an example of what a `README.md` for your todo list application could look like:
+
+---
+
+# Todo List Application
+
+A simple and interactive todo list application built with Flask and MongoDB. This application allows users to register, login, add, edit, complete, and delete todo tasks. Additionally, users can export their completed tasks to a CSV file. Tasks can also be tagged with an alarm, which sends a reminder when the task's due date arrives.
+
+## Features
+
+- User Registration and Login.
+- Add tasks with details like priority level, tags, creation and due date.
+- Edit existing tasks.
+- Mark tasks as complete.
+- Delete tasks.
+- View only completed tasks.
+- Export completed tasks to CSV.
+- Receive reminders on tasks' due date if alarm is set.
+
+## Installation
+
+- Clone the repository.
+- Install the dependencies using pip:
+    ```
+    pip install -r requirements.txt
+    ```
+- Run the app:
+    ```
+    python app.py
+    ```
+
+## Technologies
+
+- [Flask](https://flask.palletsprojects.com/en/1.1.x/): A micro web framework written in Python.
+- [PyMongo](https://pymongo.readthedocs.io/en/stable/): MongoDB driver for Python.
+- [Bcrypt](https://pypi.org/project/bcrypt/): Library for password hashing.
+- [APScheduler](https://apscheduler.readthedocs.io/en/stable/): Python scheduling library.
+- [MongoDB Atlas](https://www.mongodb.com/cloud/atlas): Cloud database service for MongoDB.
+
+## The application is deployed on heroku
+https://vast-sierra-41395.herokuapp.com/
+## The following screenshots show the working of the application
+
+1. First the user registers for the application
+![img.png](img.png)
+2. Then the users logs in to the app
+![img_2.png](img_2.png)
+3. The User will add particular task
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
+![img_5.png](img_5.png)
+4. There is a functionality to add the task, Mark it as complete edit or delete it
+![img_6.png](img_6.png)
+5. There is also an functionality to view and export the complete tasks into a csv file
+![img_7.png](img_7.png)
+![img_8.png](img_8.png)
+6. No user is allowed to login without registering
+7. Duplicate users are not allowed
+![img_1.png](img_1.png)
---
+++ app.py
@@ -0,0 +1,189 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from flask_pymongo import PyMongo
+from flask import redirect, url_for
+from bcrypt import hashpw, checkpw, gensalt
+import csv
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
+
+# App initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config["MONGO_URI"] ="mongodb+srv://admin:test@cluster0.2pkodqh.mongodb.net/Todo"
+mongo = PyMongo(app)
+
+# Scheduler initialization
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+def send_reminder(username, todo_item):
+    # Fetch user from MongoDB
+    user = mongo.db.users.find_one({'username': username})
+
+    # If user found, loop through todos and flash a reminder for matching todo
+    if user:
+        todos = user['todos']
+        for todo in todos:
+            if todo['item'] == todo_item:
+                flash(f"Reminder: {todo_item} (Due: {todo['due_date']} {todo['due_time']})", 'info')
+                break
+
+# The main route rendering the index page
+@app.route('/')
+def index():
+
+
+    # Initialize empty list for todos
+    todos = []
+
+    # Fetch user from session and load todos
+    if 'username' in session:
+        user = mongo.db.users.find_one({'username': session['username']})
+        if user:
+            todos = user['todos']
+            return render_template('index.html', todos=todos)
+
+    return redirect(url_for('register'))
+# Route for user registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # If request method is POST, register new user
+    if request.method == 'POST':
+        existing_user = mongo.db.users.find_one({'username': request.form['username']})
+        if existing_user is None:
+            hashed_password = hashpw(request.form['password'].encode('utf-8'), gensalt())
+            mongo.db.users.insert_one({'username': request.form['username'], 'password': hashed_password, 'todos': []})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        return 'Username already exists!'
+    return render_template('register.html')
+
+# Route for user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # If request method is POST, login user
+    if request.method == 'POST':
+        user = mongo.db.users.find_one({'username': request.form['username']})
+        if user and checkpw(request.form['password'].encode('utf-8'), user['password']):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        flash('Invalid username or password!', 'error')
+    return render_template('login.html')
+
+# Route for user logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+# Route for adding todo
+@app.route('/add', methods=['POST'])
+def add():
+    if 'username' in session:
+        # Create todo_item dict from form data
+        todo_item = {
+            'item': request.form['item'],
+            'completed': False,
+            'priority': request.form['priority'],
+            'tags': [tag.strip() for tag in request.form.get('tags', '').split(',')],
+            'created_date': request.form['created_date'],
+            'due_date': request.form['due_date'],
+            'alarm': 'alarm' in request.form
+        }
+
+        # If alarm is set, add reminder job to scheduler
+        if todo_item['alarm']:
+            reminder_datetime = datetime.datetime.combine(
+                datetime.datetime.strptime(todo_item['due_date'], '%Y-%m-%d').date(),
+                datetime.datetime.now().time()
+            )
+            scheduler.add_job(lambda: send_reminder(session['username'], todo_item['item']), 'date', run_date=reminder_datetime)
+
+        # Update MongoDB document
+        mongo.db.users.update_one(
+            {'username': session['username']},
+            {'$push': {'todos': todo_item}}
+        )
+    return redirect(url_for('index'))
+
+# Route for marking a todo as completed
+@app.route('/complete/<string:todo_item>')
+def complete(todo_item):
+    if 'username' in session:
+        mongo.db.users.update_one(
+            {'username': session['username'], 'todos.item': todo_item},
+            {'$set': {'todos.$.completed': True}}
+        )
+    return redirect(url_for('index'))
+
+# Route for deleting a todo
+@app.route('/delete/<string:todo_item>')
+def delete(todo_item):
+    if 'username' in session:
+        mongo.db.users.update_one(
+            {'username': session['username']},
+            {'$pull': {'todos': {'item': todo_item}}}
+        )
+    return redirect(url_for('index'))
+
+# Route for editing a todo
+@app.route('/edit/<string:todo_item>', methods=['GET', 'POST'])
+def edit(todo_item):
+    if 'username' in session:
+        user = mongo.db.users.find_one({'username': session['username'], 'todos.item': todo_item})
+        if user:
+            if request.method == 'POST':
+                # Update MongoDB document with form data
+                mongo.db.users.update_one(
+                    {'username': session['username'], 'todos.item': todo_item},
+                    {
+                        '$set': {
+                            'todos.$.item': request.form['item'],
+                            'todos.$.priority': request.form['priority'],
+                            'todos.$.tags': [tag.strip() for tag in request.form.get('tags', '').split(',')],
+                            'todos.$.created_date': request.form['created_date'],
+                            'todos.$.due_date': request.form['due_date'],
+                            'todos.$.alarm': 'alarm' in request.form
+                        }
+                    }
+                )
+                return redirect(url_for('index'))
+            else:
+                # If request method is GET, render edit page with todo
+                todo = next(todo for todo in user['todos'] if todo['item'] == todo_item)
+                return render_template('edit.html', todo=todo)
+    return redirect(url_for('login'))
+
+# Route for viewing completed todos
+@app.route('/completed')
+def completed():
+    if 'username' in session:
+        user = mongo.db.users.find_one({'username': session['username']})
+        if user:
+            completed_todos = [todo for todo in user['todos'] if todo['completed']]
+        else:
+            completed_todos = []
+        return render_template('completed.html', completed_todos=completed_todos)
+    return redirect(url_for('login'))
+
+# Route for exporting completed todos to CSV
+@app.route('/export_csv')
+def export():
+    if 'username' in session:
+        user = mongo.db.users.find_one({'username': session['username']})
+        if user:
+            completed_todos = [todo for todo in user['todos'] if todo['completed']]
+            filepath = os.path.join(app.root_path, f"{session['username']}_completed_tasks.csv")
+            with open(filepath, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Task', 'Priority', 'Tags', 'Created Date', 'Due Date', 'Alarm'])
+                for todo in completed_todos:
+                    writer.writerow([todo['item'], todo['priority'], ', '.join(todo['tags']),
+                                     todo['created_date'], todo['due_date'], todo['alarm']])
+            return send_file(filepath, as_attachment=True, mimetype='text/csv', attachment_filename=os.path.basename(filepath))
+    return redirect(url_for('login'))
+
+# Main driver function
+if __name__ == '__main__':
+    app.run(debug=True)
